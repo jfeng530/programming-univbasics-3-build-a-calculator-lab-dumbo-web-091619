5d9afa836d21d633ffb6e5f31bd9ac7d24334755
Jacky Feng <jackyfeng530@yahoo.com>
2019-08-20 04:12:03 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,20 +4,23 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  difference = num2 - num1
+  difference = num1 - num2
   return difference
 end
 
 def division(num1, num2)
-  
+  quotient = num1 / num2
+  return quotient
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  return product
 end
 
 def modulo(num1, num2)
-
+  quotient = num1 / num2
+  
 end
 
 def square_root(num)
