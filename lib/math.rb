4afb23560ed8857cf4f785e8fb232851ada04e91
Jacky Feng <jackyfeng530@yahoo.com>
2019-08-20 04:10:59 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,14 +1,15 @@
 def addition(num1, num2)
   sum = num1 + num2
-  sum
+  return sum
 end
 
 def subtraction(num1, num2)
-
+  difference = num2 - num1
+  return difference
 end
 
 def division(num1, num2)
-
+  
 end
 
 def multiplication(num1, num2)
